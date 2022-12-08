a2b51d0b245756f7621bd51e9cd012645005f0e7
wakjirabekele <wakjirabekele2018.com>
2022-12-07 23:37:32 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
+/*
  *
- *  * main - entry point
+ *   main - entry point
  *
- *   * Return: 0
+ *    Return: 0
  *
- *    */
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
