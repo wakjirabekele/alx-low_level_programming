0d4f5b984b8ef01ffff891c327997575379e4351
wakjirabekele <wakjirabekele2018.com>
2022-12-08 00:45:37 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  *main - Entry point
- * 
- *Return: Always 0 (Success)                                               
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
