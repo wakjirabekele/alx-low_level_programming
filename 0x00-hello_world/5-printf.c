60a846a020d45a426d53b60b30fe49b81592a58f
wakjirabekele <wakjirabekele2018.com>
2022-12-08 00:03:52 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
+ *main - entry point
  *
- *  * main - entry point
- *
- *   * Return: 0
- *
- *    */
+ *Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
