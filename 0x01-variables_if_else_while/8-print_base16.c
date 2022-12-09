c8a6fc7549310ed0921921f5b90d65c148919530
wakjirabekele <wakjirabekele2018.com>
2022-12-09 01:24:59 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char x1;
+
+	for (x = 0; x <= 9; x++)
+	printf("%i", x);
+	for (x1 = 'a'; x1 <= 'f'; ++x1)
+	putchar(x1);
+
+	putchar('\n');
+
+	return (0);
+}
