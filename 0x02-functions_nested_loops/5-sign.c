5df00b7a3fe1dde472ed3ea4a1035d2a96917224
wakjirabekele <wakjirabekele2018.com>
2022-12-13 01:17:25 -0800

    my commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - print the sign of number
+ *@n: the number tha will be given
+ *Return: 1 if greate than zero, 0 if equal to zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
