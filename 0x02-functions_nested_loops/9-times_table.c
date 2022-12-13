31d0b37f0057c27714bde9950a0a374bca647dc2
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2022-12-13 15:34:55 +0300

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,31 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, res;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			k = i * j;
-													if (j == 0)
-			{
-				_putchar(k + '0');
-													}
-			else if (k <= 9)
-			{
-														_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-													}
-			else if (k >= 10)
+			res = (i * j);
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-													}
-												}
-	_putchar('\n');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
