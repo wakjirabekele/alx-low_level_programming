1080e6e8810885d8a055ff94aa5b8a071bea24a0
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2022-12-15 10:06:45 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - print function
+ * @n: input variable
+ *
+ * Return: always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+	print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
