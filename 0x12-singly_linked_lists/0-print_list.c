3b896b41a0648f01b9d07416df73119510efba47
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-01-27 15:16:29 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ nelem = 0;
 
 while (h != NULL)
 {
-	if (h->str == NULL)
-	printf("[%d] %s\n", 0, "(nil)");
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
 
-	else
-	printf("[%d] %s\n", h->len, h->str);
-	h = h->next;
-	nelem++;
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nelem++;
 }
 
 return (nelem);
